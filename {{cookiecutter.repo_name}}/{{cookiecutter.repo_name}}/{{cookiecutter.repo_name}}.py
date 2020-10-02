f2d75cbe64e4e974570bb9db5ec705d6c5c36451
Phamduc <phamduc0701@gmail.com>
2020-10-02 09:07:34 +0700

    + Show Image in Kivy

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}.py
@@ -1,9 +1,14 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.uix.image import Image
 
 
 class MyGrid(Widget):
+    pass
+
+
+class MyImage(Image):
     pass
 
 
