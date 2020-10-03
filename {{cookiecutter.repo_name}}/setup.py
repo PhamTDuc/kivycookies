c17204073e4c562871828fb12f15fd118d330452
Phamduc <phamduc0701@gmail.com>
2020-10-03 21:34:17 +0700

    - Fix bad resrouces locate

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -40,16 +40,15 @@ setup(
         "templates, example, documentation, tutorial, setup.py, package, "
         "android, touch, mobile, NUI"
     ),
-    url='https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.repo_name}}',
     install_requires=['kivy>=1.8.0'],
     zip_safe=False,
     packages=find_packages(),
     package_data={
-        '{{cookiecutter.repo_name}}': ['*.kv*']
+        '{{cookiecutter.repo_name}}': ['*.kv*','res/*.png'],
     },
     entry_points={
         'console_scripts': [
-            '{{cookiecutter.repo_name}}={{cookiecutter.repo_name}}.main:main'
+            '{{cookiecutter.repo_name}}={{cookiecutter.repo_name}}.__main__:main'
         ]
     },
     tests_require=['pytest'],
---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from kivykivy.kivykivy import HelloKivyApp
+
+
+def main():
+    HelloKivyApp().run()
+
+
+if __name__ == '__main__':
+    main()
